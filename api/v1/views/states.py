ee0dbb6f2892ed22ddedbc983aba53718d4ed5f1
Praise Aribisala <aribisalapraise12@gmail.com>
2024-08-04 16:19:58 +0100

    Updates on states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,6 +6,7 @@ that handles all default RESTFul API actions
 
 from flask import jsonify
 from api.v1.views import app_views
+from model import storage
 
 @app_views.route('/states')
 def all_objects():
@@ -26,9 +27,9 @@ def state_objects(state_id):
         obj_list = [item for item in obj if state_id == item.id]
         return (obj_list[0])
     else:
-        return jsonify({
+        return (jsonify({
             "error": "Not Found"
-            })
+            }))
 
 @app_views.route('/states/<state_id>', method=['DELETE'])
 def handle_delete(state_id):
@@ -39,13 +40,44 @@ def handle_delete(state_id):
         obj = state_objects(state_id)
         storage.delete(obj)
         result = {}
-        result.status_code = 200
+#        result.status_code = 200
         return (result)
     else:
-        return jsonify({"error": "Not Found"})
+        return (jsonify({"error": "Not Found"}), 200)
 
 @app_views.route('/states', method=['POST'])
 def create_state():
-    request_data = request.get_json
+    """
+    Creates a new state
+    """
+    if request.is_json:
+        data = request.get_json
+        state_id = data['id']
+        if 'name' in data.keys():
+            storage.new(data)
+            state_obj = state_objects(state_id)
+            return (jsonify(state_obj), 201)
+        else:
+            return (jsonify({"error": "Missing name"}), 400)
+    else:
+        return (jsonify({
+            "error": "Not a JSON"
+            }), 400)
 
 
+@app_views.route('states/<state_id>', method=['PUT'])
+def update_states(state_id):
+    """
+    Updates the states obj
+    """
+    if request.is_json:
+        data = request.get_json
+        for obj in storage.__objects:
+            if state_id == obj.id:
+                obj['name'] = data['name']
+                state_obj = state_objects(state_id)
+                return (jsonify(state_obj), 200)
+            else:
+                return (jsonify({"error": "Not Found"}))
+    else:
+        return (jsonify({"error": "Not a json"}), 404)
