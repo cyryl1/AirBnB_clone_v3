ad13efdc8193e0d704390dbd84d5232a3413a545
Akinwalee <obatulafuad@gmail.com>
2024-08-05 06:00:09 +0000

    Handle update in city view

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -8,20 +8,20 @@ from api.v1.views import app_views
 
 
 @app_views.route("/api/v1/states/<state_id>/cities",
-        methods=["GET"])
+                 methods=["GET"])
 def all_cities(state_id):
     """Returns all Cities with
     the specified state_id"""
 
     cities = [i.to_dict for i in storage.all(cls='City').values()
-            if i.to_dict["state_id"] = state_id]
+              if i.to_dict["state_id"] == state_id]
     if len(cities) < 1:
         return (redirect("/api/v1/nop"))
     return (jsonify(cities))
 
 
 @app_views.route("/api/v1/cities/<city_id>",
-        methods=["GET"])
+                 methods=["GET"])
 def get_city(city_id):
     """Return a City object with the specified id"""
 
@@ -32,7 +32,7 @@ def get_city(city_id):
 
 
 @app_views.route("/api/v1/cities/<city_id>",
-        methods=["DELETE"])
+                 methods=["DELETE"])
 def delete_city(city_id):
     """ Deletes the city object
     with the specified ID"""
@@ -45,7 +45,7 @@ def delete_city(city_id):
 
 
 @app_views.route("/api/v1/states/<state_id>/cities",
-        methods=["POST"])
+                 methods=["POST"])
 def create_city(state_id):
     """Creates a new City object based on
     the specified state_id"""
@@ -65,3 +65,21 @@ def create_city(state_id):
     model.save()
     return (jsonify(model.to_dict()))
 
+
+@app_views.route("/api/v1/cities/<city_id>",
+                 methods=["PUT"])
+def update_city(city_id):
+    """Update the City object with the
+    specified ID"""
+
+    city = storage.get('City', city_id)
+    if city is None:
+        return (redirect("/api/v1/nop"))
+    data = request.get_json()
+    if not data.is_json():
+        return (jsonify({
+            "error": "Not a JSON"
+            }), 400)
+    setattr(city, data["name"])
+    city.save()
+    return (jsonify(city.to_dict()))
